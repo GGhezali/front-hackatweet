1b9f3d050f692fcf1ece89694ef728735f99f0da
Nassim <nassim.hiber@gmail.com>
2025-03-27 13:57:43 +0100

    feat: modification style Tweet

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,22 +1,33 @@
-import styles from '../styles/Tweet.module.css';
+import styles from "../styles/Tweet.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
 
 function Tweet() {
+  const dispatch = useDispatch();
+  const users = useSelector((state) => state.user.value);
+  const [contenu, setContenu] = useState([]);
+  console.log(users)
+
   return (
     <div className={styles.tweet}>
-        <div className={styles.title}>
-            <h1>HOME</h1>
+      <div className={styles.title}>
+        <h1>HOME</h1>
+      </div>
+      <div className={styles.content}>
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="What's up?"
+          onChange={(e) => setContenu(e.target.value)}
+          value={contenu}
+        ></input>
+      </div>
+      <div className={styles.submit}>
+        <div>0/280</div>
+        <div>
+          <button className={styles.button}>Tweet</button>
         </div>
-        <div className={styles.content} >
-            <input className={styles.input} type="text" placeholder="What's up?"></input>
-        </div>
-        <div className={styles.submit}>
-            <div>0/280</div>
-            <div>
-                <button className={styles.button}>Tweet</button>
-            </div>
-        </div>
-
-
+      </div>
     </div>
   );
 }
