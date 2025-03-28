ef797f0cd916a80a05955f257359fbe1926f088d
Nassim <nassim.hiber@gmail.com>
2025-03-28 09:45:47 +0100

    feat: modif css + trends

--- components/Trends.js
+++ components/Trends.js
@@ -3,6 +3,10 @@ import { useSelector, useDispatch } from 'react-redux';
 
 function Trends({ hashtag, count}) {
 
+  let tweet = "Tweet"
+if (count > 1) {
+  tweet = "Tweets"
+}
 
   return (
     <div className={styles.trends}>
@@ -10,7 +14,7 @@ function Trends({ hashtag, count}) {
             <h4>{hashtag}</h4>
         </div>
         <div>
-            <span className={styles.grey}><span>{count}</span> Tweets</span>
+            <span className={styles.grey}><span>{count}</span> {tweet}</span>
         </div>
 
     </div>
